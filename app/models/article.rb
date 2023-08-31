8182b3769dfb9167c5fabe1bc1c69185d54d1017
Ali Haider <aliprogrammer309@gmail.com>
2023-08-31 13:43:10 +0500

    added assocation between users and articles i.e one-to-many

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: {minimum:5, maximum:30}
-    validates :description, presence: true, length: {minimum:15, maximum:300}
+  belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 30 }
+  validates :description, presence: true, length: { minimum: 15, maximum: 300 }
 end
