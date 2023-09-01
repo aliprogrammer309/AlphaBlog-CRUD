161034e9fe68333747defee6da05e4e002dea339
Ali Haider <aliprogrammer309@gmail.com>
2023-09-01 14:48:35 +0500

    added the admin functionality and user restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
     @article = Article.find(params[:id])
   end
@@ -40,5 +43,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 end
